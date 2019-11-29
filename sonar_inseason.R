f6fbd5277237f10c566f731a926df68febbf0b8a
Davidson <DAVIDSONKA@dfo-mpo.ca>
2019-11-29 15:43:27 -0800

    cleaned inseason

--- sonar_inseason.R
+++ sonar_inseason.R
@@ -2,17 +2,8 @@
 # ALLO! 
 
 # Here is some code to replicate the "Inseason_reporting" tab in Excel sonar tools
-# changes are committed to khdavidson/fraser-sox/sonar_inseason repo
+# changes are committed to khdavidson/fraser-sox/ repo
 
-###############################
-# STEPS PRECEDING THIS SCRIPT #
-###############################
-
-  # Obviously there were some steps taken before this. 
-  # First, export the 'Data entry' and 'Environmental Data' tabs (as-is) as independent .csv files. It is important you make no formatting changes, as the following code is based on cleaning and re-formatting those current formats.
-  # Save them wherever you want to reference from - the exact location doesn't matter. 
-
-  # The following code will replicate the 'Inseason_reporting' tab, and maybe eventually also produce some pretty graphs (TBD...)
 
 ##########
 # SET UP #
@@ -26,18 +17,22 @@ library(XLConnect)
 library(xlsx)
 library(openxlsx)
 
-# set working directory where spreadsheet is
+# set working directory where Excel data entry file is
 setwd("C:/DFO-MPO/Data")
 
-##############
-# Count data #
-##############
+#############################
+# LOAD and CLEAN EXCEL DATA #
+#############################
+
+#####
+# Count data
+#####
 
 # read in COUNT data 
-counts <- read.xlsx("Stellako Sonar_tool_2019.xlsm", sheet = 7, startRow=5, cols=c(1:12), colNames=T, detectDates=T)
+counts <- read.xlsx("Stellako Sonar_tool_2019.xlsm", sheet=7, startRow=5, cols=c(1:12), colNames=T, detectDates=T)
                                                                                             
 # reformat COUNT dataframe 
-counts <- counts %>%                                                  # rename columns to be more R friendly
+counts <- counts %>%                                                                         # rename columns to be more R friendly
   rename(bank = Bank,
          observer = Observer,
          date = Date,
@@ -50,24 +45,65 @@ counts <- counts %>%                                                  # rename c
          ch_ds = CH_ds,
          count_number = `Obs.Count.#`,
          comments = Comments) %>%
-  mutate_at(vars(c(4)), funs(as.numeric))                             # reformat some integers to be numeric
+  mutate_at(vars(c(4)), funs(as.numeric))                                                    # reformat some integers to be numeric
 
 # re-arrange COUNT data for easy visualizaton, remove the one training count, add net column 
 counts <- counts %>%
   arrange(date, count_hr_24) %>%                                                             # ordered by date, and then count hour (1-24)
-  filter(count_number != "NA") %>%
-  mutate(sox_us_net = sox_us-sox_ds) %>%
-  mutate(date_hr = paste(paste(gsub("-", "-", date)), count_hr_24, sep="-"))
+  filter(count_number != "NA") %>%                                                           # remove rows where count_number is an NA 
+  mutate(sox_us_net = sox_us-sox_ds) %>%                                                     # create new column "sox_us_net" to calculate net upstream movement
+  mutate(date_hr = paste(paste(gsub("-", "-", date)), count_hr_24, sep="-"))                 # create new column "date_hr" to create unique date-count hour variable       
 
 
+#####
+# Environmental data
+#####
+
+# read in ENV data  
+enviro <- read.xlsx("Stellako Sonar_2018.xlsx", sheet=2, startRow=3, cols=c(1:13), colNames=T, detectDates=T)
+
+# reformat ENV dataframe 
+enviro <- enviro %>%                                                                        # rename columns to be more R friendly
+  rename(date = `Date.(dd/mm/yy)`,
+         observer_1 = Observer1, 
+         observer_2 = Column1,
+         tod = Column2,
+         gauge_m = `Gauge.(m)`,
+         bankfull_p = `%Bankfull`,
+         brightness = Brightness,
+         cloud_p = `%Cloudy`,
+         precip_type = PrecType,
+         precip_int = PrecInt,
+         fish_vis = FishVis,
+         water_temp = WTemp,
+         water_clarity = WClarity) %>%
+  filter(date != "", date != "**sonar pulled 0820 Oct 2, 2018") %>%                        # remove cases where date is blank, and the last row that had an anecdotal comment 
+  mutate(date = lubridate::ymd(date)) %>%                                                  # reformat date to be date format
+  mutate_at(vars(c(5, 12)), funs(as.numeric))                                              # reformat some variables to be numeric
 
 
+####################
+# IN-SEASON REPORT #
+####################
 
+# summarize by date, expanded based on number of files counted 
+t0 <- counts.avg %>% 
+  group_by(date) %>% 
+  summarize(total_us = sum(avg_net, na.rm=T), n_files = n()) %>%
+  mutate_at(vars(c(3)), funs(as.numeric)) %>%
+  mutate(daily_net_exp = as.numeric(ifelse(n_files=="24", total_us*3,
+                                ifelse(n_files=="12", total_us*6,
+                                       ifelse(n_files=="9", total_us*9, ""))))) %>%
+  mutate_at(4, funs(round(.,0))) %>%
+  print(t0)
 
+# join with environmental data - recall enviro dataframe is 'e.raw'
+t1 <- left_join(t0, enviro, by ='date')
 
-
-
-
+# omit unecessary columns and re-order remaining columns
+t1 <- t1 %>%
+  select(-c(5:7, 9:14, 16)) %>%
+  select(date, water_temp, gauge_m, n_files, daily_net_exp) 
 
 
 
@@ -89,77 +125,6 @@ counts.cv <- counts %>%
   summarize(net_mean = mean(sox_us_net), net_sd = ifelse(sox_us_net=0,0,sd(sox_us_net)), cv = ifelse(net_sd>0, abs(net_sd/net_mean), 0)) %>% 
   print()
 
-
-######################
-# Environmental data #
-######################
-
-# read in ENV data  
-enviro <- read.xlsx("Stellako Sonar_2018.xlsx", sheet = 2, startRow=3, cols=c(1:13), colNames=T, detectDates=T)
-
-# reformat ENV dataframe 
-enviro <- enviro %>%                                                                           # rename columns to be more R friendly
-  rename(date = `Date.(dd/mm/yy)`,
-         observer_1 = Observer1, 
-         observer_2 = Column1,
-         tod = Column2,
-         gauge_m = `Gauge.(m)`,
-         bankfull_p = `%Bankfull`,
-         brightness = Brightness,
-         cloud_p = `%Cloudy`,
-         precip_type = PrecType,
-         precip_int = PrecInt,
-         fish_vis = FishVis,
-         water_temp = WTemp,
-         water_clarity = WClarity) %>%
-  filter(date != "", date != "**sonar pulled 0820 Oct 2, 2018") %>%
-  mutate(date = lubridate::ymd(date)) %>%                                                    
-  mutate_at(vars(c(5, 12)), funs(as.numeric))
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#######################
-# PRELIM CALCULATIONS #
-#######################
-
-
-
-
-
-########################
-# CREATE SUMMARY TABLE #
-########################
-
-# summarize by date, expanded based on number of files counted 
-t0 <- counts.avg %>% 
-  group_by(date) %>% 
-  summarize(total_us = sum(avg_net, na.rm=T), n_files = n()) %>%
-  mutate_at(vars(c(3)), funs(as.numeric)) %>%
-  mutate(daily_net_exp = as.numeric(ifelse(n_files=="24", total_us*3,
-                                ifelse(n_files=="12", total_us*6,
-                                       ifelse(n_files=="9", total_us*9, ""))))) %>%
-  mutate_at(4, funs(round(.,0))) %>%
-  print(t0)
-
-# join with environmental data - recall enviro dataframe is 'e.raw'
-t1 <- left_join(t0, enviro, by ='date')
-
-# omit unecessary columns and re-order remaining columns
-t1 <- t1 %>%
-  select(-c(5:7, 9:14, 16))%>%
-  select(date, water_temp, gauge_m, n_files, daily_net_exp) 
 
 
 
